9b12ff44b1ab69ff6552bf4158ee395df2df0647
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-09-21 18:33:07 +0300

    #11: Add ability system to modules and character class

--- Source/GAS/CharacterBase.h
+++ Source/GAS/CharacterBase.h
@@ -3,26 +3,28 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
 #include "GameFramework/Character.h"
 #include "CharacterBase.generated.h"
 
 UCLASS()
-class GAS_API ACharacterBase : public ACharacter
+class GAS_API ACharacterBase : public ACharacter, public IAbilitySystemInterface
 {
     GENERATED_BODY()
 
 public:
-    // Sets default values for this character's properties
     ACharacterBase();
 
 protected:
-    // Called when the game starts or when spawned
     virtual void BeginPlay() override;
 
 public:
-    // Called every frame
     virtual void Tick(float DeltaTime) override;
-
-    // Called to bind functionality to input
     virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = AbilitySystem)
+    class UAbilitySystemComponent* AbilitySystemComponent;
+
+    virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override { return AbilitySystemComponent; };
+    
 };
