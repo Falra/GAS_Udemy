773c6dcdc8cc6ddbadcb2f4882312ac8328aef5d
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-10-18 08:31:54 +0300

    #63: Add blank class for new target data

---
+++ Source/GAS/GATargetActorGetAround.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbilityTargetActor.h"
+#include "GATargetActorGetAround.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_API AGATargetActorGetAround : public AGameplayAbilityTargetActor
+{
+    GENERATED_BODY()
+
+};
